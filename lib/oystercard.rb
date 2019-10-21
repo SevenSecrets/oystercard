4258f8f59acf560d6f28bdb10c4782c58181d898
Student <student@Makerss-MacBook-Pro.local>
2019-10-21 14:58:54 +0100

    top up limit set and working

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,12 +1,15 @@
 class Oystercard
 
   attr_reader :balance
+  attr_reader :max_balance
 
   def initialize
     @balance = 0
+    @max_balance = 90
   end
 
   def top_up(num)
+    fail "Maximum balance of #{@max_balance} exceeded" if @balance + num > @max_balance
     @balance += num
   end
 
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -8,8 +8,14 @@ describe Oystercard do
     expect(card.balance).to eq 0
   end
 
-  it "lets you top up the balance" do
-    expect{ card.top_up(1) }.to change { card.balance }.by(1)
-  end
 
+  describe Oystercard.new do
+    it "lets you top up the balance" do
+      expect{ subject.top_up(1) }.to change { subject.balance }.by(1)
+    end
+
+    it "won't let you top up to more than 90" do
+      expect { subject.top_up(90) }.to raise_error "Maximum balance of #{subject.max_balance} exceeded"
+    end
+  end
 end
